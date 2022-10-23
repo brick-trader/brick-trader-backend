f08fe93ee2f7d0224d9f922fa39628c9c8fedaa0
ppodds <oscar20020629@gmail.com>
2022-10-23 09:28:47 +0800

    fix: prisma migrate failed

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,17 @@ import {
 } from "@nestjs/platform-fastify";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-import { promisify } from "util";
 import { exec } from "child_process";
 
 async function bootstrap() {
   if (process.env.NODE_ENV === "production") {
-    const promisifiedExec = promisify(exec);
-    await promisifiedExec("pnpm prisma migrate deploy");
+    exec("pnpm prisma migrate deploy", (err, stdout) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(stdout);
+    });
   }
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
